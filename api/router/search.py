fa3a43ca21ae5df891e6aee6b3089eb92080d540
evg1010 <vazquezgarciaeva10@gmail.com>
2024-10-16 02:44:12 -0400

    feat: search endpoint is now using hybrid search and the router was added to the FastAPI app

--- api/router/search.py
+++ api/router/search.py
@@ -1,26 +1,31 @@
 from fastapi import APIRouter, Depends, Query
 from starlette.responses import JSONResponse
 
-from api.dependencies import check_connector_credentials
-from modules.connectors import BaseConnector
+from api.dependencies import check_connector_credentials, validate_token
 from modules.vectorizers import FaissVectorizer
+
 
 router = APIRouter(prefix="/search", tags=["search"])
 
 
-@router.get("/")
+@router.get(
+    "/{storage_bucket_name}",
+    dependencies=[Depends(validate_token), Depends(check_connector_credentials)],
+)
 async def search_similarity_docs(
-    client: BaseConnector = Depends(check_connector_credentials),
+    storage_bucket_name: str,
     query: str = Query(...),
 ):
     try:
-        vectorstore = FaissVectorizer(index_name=client.username)
-        docs = vectorstore.search_similarity(query=query)
+        vectorstore = FaissVectorizer(index_name=storage_bucket_name)
 
-        # Convert the list of Document objects to dictionary representation
-        serialized_docs = [doc.__dict__ for doc in docs]
+        # docs = vectorstore.search_similarity(query=query)
 
-        return serialized_docs
+        # # Convert the list of Document objects to dictionary representation
+        # serialized_docs = [doc.__dict__ for doc in docs]
+
+        # return serialized_docs
+        return vectorstore.hybrid_search(query, use_bm25_search=True)
 
     except Exception as e:
         return JSONResponse(
